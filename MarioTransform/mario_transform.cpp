90e06a11078cc9b30760886a2440490b17d6cfe8
IshantSehrawat2005 <ishantsehrawatjs@gmail.com>
2025-02-21 03:37:09 +0530

    Mario and Trnsformation

---
+++ MarioTransform/mario_transform.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T; 
+
+    while (T--) {
+        int X;
+        cin >> X;  
+    
+        if (X % 3 == 0) {
+            cout << "NORMAL" << endl;
+        } else if (X % 3 == 1) {
+            cout << "HUGE" << endl;
+        } else {
+            cout << "SMALL" << endl;
+        }
+    }
+
+    return 0;
+}
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;  
+    
+        if (X % 3 == 0) {
+            cout << "NORMAL" << endl;
+        } else if (X % 3 == 1) {
+            cout << "HUGE" << endl;
+        } else {
+            cout << "SMALL" << endl;
+        }
+    }
+
+    return 0;
+}
